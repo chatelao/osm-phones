5736e7f12994c746eeacc9442d389acecc83dbba
chatelao <olivier.chatelain@gmail.com>
2019-11-20 22:29:06 +0100

    Update phones.sql

--- pgplsql/phones.sql
+++ pgplsql/phones.sql
@@ -1,13 +1,14 @@
 /*
 https://dbfiddle.uk/?rdbms=postgres_10&fiddle=fb942c970228577826f80aba53c8d71e
 */
-CREATE or REPLACE FUNCTION osmabbrv_phone(longname text) RETURNS TEXT AS $$
+CREATE or REPLACE FUNCTION osmabbrv_get_msidin(phone_number text, country text default null) RETURNS TEXT AS $$
 DECLARE
-  normalize text;
+  result text;
  BEGIN
-  abbrev=longname;
-  abbrev=replace_regex('(0)','',abbrev);
-  abbrev=replace_regex('[-/ ]','',abbrev);
-  return abbrev;
+  result=phone_number;
+  -- if regexp_matches('^+', phone_number)
+  result=replace_regex('(0)','',abbrev);
+  result=replace_regex('[-/ ]','',abbrev);
+  return result;
  END;
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
